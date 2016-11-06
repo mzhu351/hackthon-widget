10a0a4ac9bf9bcbd9e8093377260bd68cf6e54ce
Min Zhu <mzhu1112@yahoo.com>
2016-11-06 09:25:10 -0800

    got json working

--- src/app/components/pop-widget/topic-list.component.js
+++ src/app/components/pop-widget/topic-list.component.js
@@ -4,21 +4,33 @@
 
 		var app = angular.module('dynaPop');
 
-		function TopicListController() {
-			var ctrl = this;
+		function fetchTopics($http) {
+			return $http.get('./topics.json')
+							.then(function(response) {
+								return response.data;
+							});
+		}
 
-			ctrl.title = 'topic list is here';
+		function TopicListController($http) {
+			var model = this;
 
-			ctrl.$onInit = function() {
+			model.title = 'topic list is here';
+
+			model.$onInit = function() {
 				console.log('Topic List Initiated!');
+				fetchTopics($http)
+					.then(function(data) {
+						console.log('onInit data', data);
+						model.topics = data;
+					});
 			};
 
 		}
 
 		app.component('topicList', {
 			templateUrl: 'app/components/pop-widget/topic-list.component.html',
-			controllerAs: 'ctrl',
-			controller: [TopicListController]
+			controllerAs: 'model',
+			controller: ['$http', TopicListController]
 		});
 
 })();
